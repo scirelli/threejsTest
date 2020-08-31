c1ea9be3909b9aab2fcaae439ceeea061b896ba5
scirelli <scirelli+git@gmail.com>
2020-08-31 00:10:20 -0400

    quick save

--- gamePhysics.js
+++ gamePhysics.js
@@ -15,23 +15,40 @@ import {
 
 const gravity = new b2Vec2(0.0, 10.0),
     DO_SLEEP = true,
-    world = new b2World(gravity, DO_SLEEP),
-    circleShape = new b2CircleShape(3),
-    fixtureDef = new b2FixtureDef(),
+    world = new b2World(gravity, DO_SLEEP);
+
+
+
+let circleShape = new b2CircleShape(1),
+    circleFixtureDef = new b2FixtureDef(),
     circleBdDef = new b2BodyDef();
 
+circleFixtureDef.shape = circleShape;
+circleFixtureDef.density = 1.0;
+circleFixtureDef.friction = 0.5;
+circleFixtureDef.restitution = 0.5;
 
-//####### Fixture ############
-fixtureDef.shape = circleShape;
-fixtureDef.density = 1.0;
-fixtureDef.friction = 0.5;
-fixtureDef.restitution = 0.5;
-
-//####### Body ##########
 circleBdDef.type = b2Body.b2_dynamicBody;
-
 let ballbody = world.CreateBody(circleBdDef);
-ballbody.CreateFixture(fixtureDef);
+ballbody.CreateFixture(circleFixtureDef);
+
+
+
+let circle2Shape = new b2CircleShape(1),
+    circle2FixtureDef = new b2FixtureDef(),
+    circle2BdDef = new b2BodyDef();
+
+circle2FixtureDef.shape = circle2Shape;
+circle2FixtureDef.density = 1.0;
+circle2FixtureDef.friction = 0.5;
+circle2FixtureDef.restitution = 0.5;
+
+circle2BdDef.type = b2Body.b2_dynamicBody;
+circle2BdDef.position = {x: 0, y: -2.5};
+let ball2body = world.CreateBody(circle2BdDef);
+ball2body.CreateFixture(circle2FixtureDef);
+
+
 
 let floorshape = new b2PolygonShape(),
     floorfixtureDef = new b2FixtureDef(),
@@ -47,35 +64,5 @@ floorbodyDef.position = {x: 0, y: 10};
 floorbodyDef.angle = 0.0;
 let floorbody = world.CreateBody(floorbodyDef);
 floorbody.CreateFixture(floorfixtureDef);
-export {world, ballbody, floorbody};
 
-
-//var jointDef = new Box2D.Dynamics.Joints.b2RevoluteJointDef();
-////jointDef.anchorPoint.Set(250, 200);
-//jointDef.bodyA = world.GetGroundBody();
-//jointDef.bodyB = circleBody;
-//world.CreateJoint(jointDef);
-
-// let polyShape = new Box2D.Collision.Shapes.b2PolygonShape();
-// polyShape.SetAsBox(1.0, 1.0);
-// let fixtureDef = new Box2D.Dynamics.b2FixtureDef();
-// fixtureDef.shape = polyShape;
-// fixtureDef.density = 1.0;
-// fixtureDef.friction = 0.3;
-
-// let bodyDef = new Box2D.Dynamics.b2BodyDef();
-// bodyDef.type = Box2D.Dynamics.b2Body.b2_dynamicBody;
-// bodyDef.position.Set(0.0, 4.0);
-
-// let body = world.CreateBody(bodyDef);
-// body.CreateFixture(fixtureDef);
-// body.SetPosition(new Box2D.Common.Math.b2Vec2(0.0, 0.0));
-// let groundBodyDef = new Box2D.Dynamics.b2BodyDef();
-// groundBodyDef.position.Set(0.0, -10.0);
-// let groundBody = world.CreateBody(groundBodyDef);
-
-// let groundBox = new Box2D.Collision.Shapes.b2PolygonShape();
-// groundBox.SetAsBox(50.0, 10.0);
-
-// groundBody.CreateFixture(new Box2D.Dynamics.b2FixtureDef(), 0.0);
-
+export {world, ballbody, floorbody, ball2body};
--- main.js
+++ main.js
@@ -1,25 +1,26 @@
 /*global THREE*/
-import {world, ballbody, floorbody} from './gamePhysics.js';
+import {world, ballbody, floorbody, ball2body} from './gamePhysics.js';
 
 const renderer = new THREE.WebGLRenderer(),
     scene = new THREE.Scene(),
     camera = new THREE.PerspectiveCamera( 45, 1, 1, 10000 ),
     mainLight = new THREE.PointLight( 0xffffff, 1.0, 500, 2 ),
     ambientLight = new THREE.AmbientLight( 0xF0F0F0 ),
-    geometry = new THREE.OctahedronGeometry(1, 1),
+    geometry = new THREE.OctahedronGeometry(1, 2),
     texture = new THREE.TextureLoader().load('textures/checker/redwhite.jpg'),
     material = new THREE.MeshStandardMaterial({
         'map':       texture,
         'roughness': 0.8
     }),
     gball = new THREE.Mesh(geometry, material),
+    gball2 = new THREE.Mesh(geometry, material),
     container = document.body.querySelector('#main-container'),
 
     cubeGeometry = new THREE.BoxGeometry(10, 1, 1),
     cubeMaterial = new THREE.MeshBasicMaterial({color: 0x00FFFF}),
     cube = new THREE.Mesh(cubeGeometry, cubeMaterial);
 
-cube.position.y = -7.5;
+cube.position.y = -floorbody.GetPosition().y;
 scene.add(cube);
 
 let translate = {x: 0, y: 0},
@@ -45,6 +46,10 @@ gball.castShadow = true;
 gball.receiveShadow = true;
 scene.add(gball);
 
+gball2.castShadow = true;
+gball2.receiveShadow = true;
+scene.add(gball2);
+
 window.addEventListener('resize', resizeCanvas);
 resizeCanvas();
 
@@ -63,7 +68,16 @@ let timeStep = 1.0/60,
     gball.position.x = pos.x;
     gball.position.y = 0-pos.y;
 
+    pos = ball2body.GetPosition(),
+    angle = ball2body.GetAngle();
+
+    gball2.rotation.x = angle;
+    gball2.rotation.y = angle;
+    gball2.position.x = pos.x;
+    gball2.position.y = 0-pos.y;
+
     console.log(pos.x, pos.y, pf.x, pf.y);
+
     renderer.render(scene, camera);
     requestAnimationFrame(animate);
 })();
